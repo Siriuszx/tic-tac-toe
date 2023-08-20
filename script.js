ceb36d2d2b71546ac08b4e24e4797a1486468f4f
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-08-20 16:56:34 +0200

    Create GameController and GameBoard main objects

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
+'use strict'
+
+const gameBoardDOM = document.querySelectorAll('.board-cell');
+
 const Player = (name, marker) => {
-    
     return {name, marker};
 };
 
 const GameBoard = (() => {
-    const gameBoardDOM = document.querySelectorAll('.board-cell');
-    let curPlayer = {};
+    const gameBoardArr = ['','','','','','','','',''];
 
-    const placeMarker = function(e) {
-        this.textContent = curPlayer.marker;
+    const updateBoard = () => {
+        for(let i = 0; i < gameBoardArr.length; i++) {
+            gameBoardDOM[i].textContent = gameBoardArr[i];
+        }
     };
 
-    gameBoardDOM.forEach(el => {
-        el.addEventListener('click', placeMarker);
-    });    
-    
-    const getCellPos = () => {
-        return Array.from(gameBoardDOM).indexOf(this);
+    const addMarker = (pos ,marker) => {
+        gameBoardArr[pos] = marker;
+        updateBoard();
     };
 
-    const updateCurPlayer = (player) => {
-        curPlayer = player;
+    return {updateBoard, addMarker};
+})();
+
+const GameController = (() => {
+    let player1 = null;
+    let player2 = null;
+
+    let turnToggle = false;  
+
+    const startGame = (p1, p2) => {
+        player1 = p1;
+        player2 = p2;
+
+        GameBoard.updateBoard();
     };
 
-    return {placeMarker, updateCurPlayer};
+    const getElPos = (el) => {
+        return Array.from(gameBoardDOM).indexOf(el);
+    };
+
+    const playTurnHandler = function(e) {
+        switch (turnToggle) {
+            case false:
+                GameBoard.addMarker(getElPos(this), 'X');
+                turnToggle = !turnToggle;
+                break;
+            case true:
+                GameBoard.addMarker(getElPos(this), 'O');
+                turnToggle = !turnToggle;
+                break;
+        }
+    };
+
+    (() => {
+        gameBoardDOM.forEach((el) => {
+            el.addEventListener('click', playTurnHandler);
+        });
+    })();
+
+    return {startGame};
 })();
 
 const player1 = Player('p1','X');
 const player2 = Player('p2','O');
-GameBoard.updateCurPlayer(player1);
+
+GameController.startGame(player1,player2);
 
 
 
