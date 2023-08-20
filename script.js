d5017fbb67390ba349671d905905ca4c51ef3e7b
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-08-20 19:57:36 +0200

    Add win conditions patterns

--- script.js
+++ script.js
@@ -6,56 +6,89 @@ const Player = (name, marker) => {
     return {name, marker};
 };
 
-const GameBoard = (() => {
-    const gameBoardArr = ['','','','','','','','',''];
+const GameBoard = (() => {//0  1  2  3  4  5  6  7  8
+    const _gameBoardArr = ['','','','','','','','',''];
+    const _markerRowPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
 
     const updateBoard = () => {
-        for(let i = 0; i < gameBoardArr.length; i++) {
-            gameBoardDOM[i].textContent = gameBoardArr[i];
+        for(let i = 0; i < _gameBoardArr.length; i++) {
+            gameBoardDOM[i].textContent = _gameBoardArr[i];
         }
     };
 
+    const getThreeInARow = (marker) => {
+        for(let i = 0; i < _markerRowPatterns.length; i++) {
+            if (_gameBoardArr[_markerRowPatterns[i][0]] &&
+                _gameBoardArr[_markerRowPatterns[i][0]] === 
+                _gameBoardArr[_markerRowPatterns[i][1]] &&
+                _gameBoardArr[_markerRowPatterns[i][1]] ===
+                _gameBoardArr[_markerRowPatterns[i][2]]) {
+                    return {
+                        patternIndex: _markerRowPatterns[i][0],
+                        result: true, 
+                        markerType: _gameBoardArr[_markerRowPatterns[i][0]], 
+                    };
+                }     
+        }
+        return {
+            result: false,
+            markerType: null,
+        };
+
+    };
+
     const addMarker = (pos ,marker) => {
-        gameBoardArr[pos] = marker;
+        _gameBoardArr[pos] = marker;
         updateBoard();
     };
 
-    return {updateBoard, addMarker};
+    return {updateBoard, addMarker, getThreeInARow};
 })();
 
 const GameController = (() => {
-    let player1 = null;
-    let player2 = null;
+    let _player1 = null;
+    let _player2 = null;
 
-    let turnToggle = false;  
+    let _turnToggle = false;  
 
     const startGame = (p1, p2) => {
-        player1 = p1;
-        player2 = p2;
+        _player1 = p1;
+        _player2 = p2;
 
         GameBoard.updateBoard();
     };
 
-    const getElPos = (el) => {
+    const _getElPos = (el) => {
         return Array.from(gameBoardDOM).indexOf(el);
     };
 
-    const playTurnHandler = function(e) {
-        switch (turnToggle) {
-            case false:
-                GameBoard.addMarker(getElPos(this), 'X');
-                turnToggle = !turnToggle;
-                break;
-            case true:
-                GameBoard.addMarker(getElPos(this), 'O');
-                turnToggle = !turnToggle;
-                break;
+    const _checkWinCondition = (marker) => {
+        
+    };
+
+    const _playTurnHandler = function(e) {
+        if(!this.textContent) {
+            console.log('Next turn');
+            switch (_turnToggle) {
+                case false:
+                    GameBoard.addMarker(_getElPos(this), 'X');
+                    _turnToggle = !_turnToggle;
+                    break;
+                case true:
+                    GameBoard.addMarker(_getElPos(this), 'O');
+                    _turnToggle = !_turnToggle;
+                    break;
+            }
         }
     };
 
     (() => {
         gameBoardDOM.forEach((el) => {
-            el.addEventListener('click', playTurnHandler);
+            el.addEventListener('click', _playTurnHandler);
         });
     })();
 
